e9779b400cb0420df5d04e4a70483350e97fba4e
chandkaurati <chandkaurati@outlook.com>
2023-12-27 11:27:49 +0530

    Route params && Handling multiple Route params

--- Express/app.js
+++ Express/app.js
@@ -10,6 +10,14 @@ const app = express()
 // this method will take two agruments 
 // first is route / route is basically where we will be requesting for our data
 // second is two  object {response}, {request} parameters
+
+const cources = [
+     {id:1, name :"jvascript"},
+     {id:2, name :"python"},
+     {id:3, name :"Java"},
+     {id:4, name :"Rust"},
+] 
+
 app.get('/', (req, res)=>{
  res.send("Got the data form the server")
 })
@@ -25,7 +33,15 @@ app.get('/contact/myinfo', (req,res)=>{
      res.send("Name: chand kaurati : Frontend SDE")
 })
 
+// Route parameters
+ app.get('/courses/:id', (req,res)=>{
+    let course = cources.find(course => course.id === parseInt(req.params.id))
+
+    if(!course)res.status(404).send("This course not available")
+    res.send(course)
+ })
 const Port =  process.env.PORT || 3000
+
 
 app.listen(Port,()=> console.log(`"the port is running on this from the Enviornmental variables  ${Port}"`))
 
--- Node moduels system/path.js
+++ Node moduels system/path.js
@@ -1,11 +1,11 @@
 const path =  require('path')
 
 
-let ext = path.extname('C:\Users\chand\OneDrive\node02\f1.txt')
+let ext = path.extname('C:\\Users\\OneDrive\\node02\\f1.txt')
 let basename = path.basename('C:\\Users\\chand\\OneDrive\\node02\\f1.txt')
 // console.log(ext)   to extract the specific path of the given file path
  
-// console.log(basename) to extract the base name fiels  NOTE : we shoud add double slashes in between the path 
+ //console.log(basename) to extract the base name fiels  NOTE : we shoud add double slashes in between the path 
 //  is's nod's beviour just for security purpuse
  
 
