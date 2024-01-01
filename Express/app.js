451d8d9684f9e87232876300ba0e97ae1ee1f13c
chandkaurati <chandkaurati@outlook.com>
2024-01-01 12:14:56 +0530

    HTTP Post method and Tested the request Postman

--- Express/app.js
+++ Express/app.js
@@ -10,6 +10,10 @@ const app = express()
 // this method will take two agruments 
 // first is route / route is basically where we will be requesting for our data
 // second is two  object {response}, {request} parameters
+ 
+// Middle weare // the middlewere is basically Express.json()
+// we just have to add this method so  tha the all data will be passed in to the Json() 
+app.use(express.json())
 
 const cources = [
      {id:1, name :"jvascript"},
@@ -17,6 +21,7 @@ const cources = [
      {id:3, name :"Java"},
      {id:4, name :"Rust"},
 ] 
+
 
 app.get('/', (req, res)=>{
  res.send("Got the data form the server")
@@ -33,17 +38,32 @@ app.get('/contact/myinfo', (req,res)=>{
      res.send("Name: chand kaurati : Frontend SDE")
 })
 
+                                 
+app.get('/cources',(req,res)=>{
+     res.send(cources)
+})
+
+app.post('/cources', (req, res)=>{
+     const cource = {
+          id :cources.length  + 1,
+          name : req.body.name  
+     }  
+     
+     cources.push(cource)
+     res.send(cource)
+})
 // Route parameters
  app.get('/courses/:id', (req,res)=>{
     let course = cources.find(course => course.id === parseInt(req.params.id))
-
-    if(!course)res.status(404).send("This course not available")
+     console.log(req.params.id)
+   if(!course)res.status(404).send("This course not available")
     res.send(course)
  })
+
+
 const Port =  process.env.PORT || 3000
 
 
 app.listen(Port,()=> console.log(`"the port is running on this from the Enviornmental variables  ${Port}"`))
 
 
- 
