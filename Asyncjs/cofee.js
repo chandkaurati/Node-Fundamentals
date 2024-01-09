81c7894502c7835027da3a44e74219f86dfc6aa6
chandkaurati <chandkaurati@outlook.com>
2024-01-09 12:52:50 +0530

    Async js

--- Asyncjs/cofee.js
+++ Asyncjs/cofee.js
@@ -3,7 +3,7 @@ function placeOrder(drink){
          if(drink === "cofee"){
              resolve("cofee ")
          }else{
-             console.log('the drink is not an cofee')   
+             console.log('the order has been rejected')   
          }
       })
 }
@@ -25,3 +25,37 @@ function ProcessOrder(order){
 // })
 
 // Solve the problem using the asyn await
+
+async function serveOrder(){
+    let orderplaced = await placeOrder('cofee')
+    console.log(orderplaced)
+    let processeorder =  await ProcessOrder(orderplaced)
+    console.log(processeorder)
+}
+
+// serveOrder()
+
+
+let input = 'Usd'
+async  function getdata(curr){
+ try {
+  let res =  await fetch(`https://api.exchangerate-api.com/v4/latest/${input}`)
+  let data =  await res.json()
+  return data 
+} catch (error) {
+     console.log(error)
+ }
+}
+
+
+let obj = getdata(input)
+function convert(obj){
+    obj.then((val)=>{ 
+        console.log(val)
+    })
+}
+
+
+convert(obj)
+
+
